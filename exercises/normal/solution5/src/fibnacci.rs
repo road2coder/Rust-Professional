8ef40a548c3397b2707380065e22400e91a7d072
r2coder <r2coder@163.com>
2025-02-15 00:59:16 +0800

    normal5: odd_fibnacci_sum via dp

--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,13 +1,17 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
+    if threshold == 0 {
+        return 0;
+    }
     let (mut first, mut second) = (0, 1);
     let mut sum = first + second;
     loop {
         let next = first + second;
         if next > threshold {
             break;
-        } else if next & 1 == 1 {
+        };
+        if next & 1 == 1 {
             sum += next;
-        }
+        };
         first = second;
         second = next;
     }
