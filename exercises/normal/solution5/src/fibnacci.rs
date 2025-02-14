acb37afa461096853c0d94f2a86ce3b2bcd2bec5
r2coder <r2coder@163.com>
2025-02-14 20:22:51 +0800

    normal5: odd_fibnacci_sum optimize

--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,15 +1,15 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
-    let mut nums = vec![1, 1];
-    let (mut first, mut second) = (1, 1);
+    let (mut first, mut second) = (0, 1);
+    let mut sum = first + second;
     loop {
-        let sum_prev_two = first + second;
-        if sum_prev_two > threshold {
+        let next = first + second;
+        if next > threshold {
             break;
-        } else if sum_prev_two & 1 == 1 {
-            nums.push(sum_prev_two);
+        } else if next & 1 == 1 {
+            sum += next;
         }
         first = second;
-        second = sum_prev_two;
+        second = next;
     }
-    nums.iter().sum()
+    sum
 }
