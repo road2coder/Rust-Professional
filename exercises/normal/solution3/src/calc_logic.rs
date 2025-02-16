390c0e54edb81e50d119bccc2ac1ad234eb441ae
r2coder <r2coder@163.com>
2025-02-16 09:53:57 +0800

    normal3: birthday

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,9 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    // 没人重复的概率为P(n) = C(365, n) = 365! / (365 - n) * 365 ^ n
+    // 使用dp解决高阶幂的问题: P(n + 1) / P(n) = (365 - n) / 365
+    let mut probability = 1.0;
+    for i in 0..n {
+        probability *= (365.0 - i as f64) / 365.0;
+    }
+    1.0 - probability
 }
